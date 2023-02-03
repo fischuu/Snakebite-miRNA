200dd6271e0589d6c7021dee00e378ab83a2e4c6
Daniel Fischer <github@danielfischer.name>
2023-02-03 16:04:09 +0200

    Update run_Snakebite-miRNA.sh

--- run_Snakebite-miRNA.sh
+++ run_Snakebite-miRNA.sh
@@ -19,7 +19,7 @@ snakemake -s $pipelineFolder/Snakebite-miRNA.smk \
           --use-singularity \
           --singularity-args "-B /scratch,/projappl,/users,/dev/shm:/tmp" \
           --configfile $projectFolder/Snakebite-miRNA_config.yaml \
-          --cluster-config $pipelineFolder/Snakebite-miRNA_server-config.yaml \
+          --cluster-config $projectFolder/Snakebite-miRNA_server-config.yaml \
           --cluster "sbatch -t {resources.time} --account={cluster.account} --gres=nvme:{cluster.nvme} --job-name={cluster.job-name} --tasks-per-node={cluster.ntasks} --cpus-per-task={threads} --mem-per-cpu={resources.mem} -p {cluster.partition} -D {cluster.working-directory}" \
           --scheduler greedy \
           $@ 
